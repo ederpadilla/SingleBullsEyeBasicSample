fd71dbeb9224b1f24a038e1de5f45f8c1bdb7dc2
€%ER Padilla! <eder.padilla97@gmail.com>
2023-09-29 10:59:28 -0600

    Add alert

--- Bull’s Eye/ViewController.swift
+++ Bull’s Eye/ViewController.swift
@@ -13,8 +13,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func didSelectHitMe(_ sender: Any) {
+        let alert = UIAlertController(
+            title: "Hello, World",
+            message: "This is my first app!",
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "Awesome",
+            style: .default,
+            handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
 }
